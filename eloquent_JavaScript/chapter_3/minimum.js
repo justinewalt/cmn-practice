57563124636061f78573dc6ff8f331f5b5bb38d4
Justin Ewalt <justinewalt@gmail.com>
2017-11-13 20:23:01 +0700

    added eloquent_javascript practice exercises

---
+++ eloquent_JavaScript/chapter_3/minimum.js
@@ -0,0 +1,9 @@
+function minimum(arg1, arg2) {
+  if (arg1 === arg2) {
+    console.log("They're equal!")
+  } else if (arg1 > arg2) {
+    console.log(arg2 + " is the lowest number!")
+  } else 
+    console.log(arg1 + " is the lowest number!")
+}
+minimum(7987987089,987978871)  
